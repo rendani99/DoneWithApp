4e96cc68d8efd55e90eecb8fe00d87825a8954c5
rendania99 <rendania460@gmail.com>
2024-03-30 01:15:58 +0200

    break

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
+//import { StatusBar } from 'expo-status-bar';
+//import { Button, StyleSheet, Text, View, Platform, StatusBar  } from 'react-native';
+//import {useDimensions} from '@react-native-community/hooks'
 
 export default function App() {
-
-  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View
+      style={{
+        backgroundColor: "dodgerblue",
+        flex: 1,
+      }}
+    ></View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+// container: {
+//   flex: 1,
+//   backgroundColor: '#fff',
+//   paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+//   // alignItems: 'center',
+//   // justifyContent: 'center',
+// },
+// });
+
+// <View style={styles.container}>
+//   <Button  title='Click Me'
+//   onPress={() => console.log("Button tapped")}
+//   />
+//</View>
